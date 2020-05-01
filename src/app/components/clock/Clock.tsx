b06a0a470b5bf2b362022ffe44ad7e2677189096
unknown <jiayuandeng0109@yahoo.com>
2020-05-01 14:30:21 -0700

    Implement clock component

--- src/app/components/clock/Clock.tsx
+++ src/app/components/clock/Clock.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 export function Clock() {
-  return <div>Clock</div>;
+  const [timeString, setTimeString] = useState("");
+
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setTimeString(getTimeString());
+    }, 1000);
+    return () => {
+      window.clearInterval(intervalId);
+    };
+  }, []);
+
+  return <div>{timeString}</div>;
+}
+
+function getTimeString() {
+  const timeString = new Date().toLocaleTimeString();
+  return timeString;
 }
